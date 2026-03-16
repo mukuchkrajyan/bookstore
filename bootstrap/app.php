d52d2ac6bc195225f4252e429c9d6cd8da2f266e
Mkrtich <mukuchkrajyan@gmail.com>
2026-03-16 18:33:21 +0400

    in-progress

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
     })
+
     ->withExceptions(function (Exceptions $exceptions): void {
         //
     })->create();
